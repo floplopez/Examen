2411814c9262703352ebfdd3ef84d6a3a592ec6c
unknown <flopez@clarity.com.ar>
2019-06-13 19:08:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
