6903478f4b9f6124b8520575546b7bc9b094997e
unknown <flopez@clarity.com.ar>
2019-06-13 21:40:55 -0300

    first commit

--- Ejercicio4/Ejercicio4.cs
+++ Ejercicio4/Ejercicio4.cs
@@ -1,1 +1,2 @@
 //El tryparse intenta pasar de un tipo de dato a otro peromitiendote darte un opcion si no puede. El parse solo intenta hacerle sin segunda opcion
+//Yo uso el Parse porque uso if para cuando puede convertir
